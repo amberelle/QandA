9363d267c4712a130841a9f0444e2070d7fac099
Amber Baird <amber.baird@gmail.com>
2016-06-10 14:27:47 -0700

    bootstrap added with bower install and in the ember-cli-build.js file

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,6 +6,9 @@ module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+    app.import('bower_components/bootstrap/dist/css/bootstrap.css.map');
+    app.import('bower_components/bootstrap/dist/js/bootstrap.js');
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
