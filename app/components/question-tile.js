6ac6834318c15ef22aa19a5c851899365aa9ddd7
Amber Baird <amber.baird@gmail.com>
2016-06-17 15:11:16 -0700

    fully functioning question and answer page with out delete button

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  
+  actions: {
+    destroyQuestion(question) {
+      if (confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('destroyQuestion', question);
+      }
+    }
+  }
+
 });
